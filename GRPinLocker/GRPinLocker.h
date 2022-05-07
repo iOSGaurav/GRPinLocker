ea4dd38ff4a5f054423e84843ffdc710bcc6ab38
Gaurav Parmar <parmargaurav069@gmail.com>
2022-05-07 18:24:11 +0530

    Initial Commit

---
+++ GRPinLocker/GRPinLocker.h
@@ -0,0 +1,18 @@
+//
+//  GRPinLocker.h
+//  GRPinLocker
+//
+//  Created by Gaurav Parmar on 07/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GRPinLocker.
+FOUNDATION_EXPORT double GRPinLockerVersionNumber;
+
+//! Project version string for GRPinLocker.
+FOUNDATION_EXPORT const unsigned char GRPinLockerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRPinLocker/PublicHeader.h>
+
+
